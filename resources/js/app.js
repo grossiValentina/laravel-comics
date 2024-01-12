95ba16b303ef958bb38d4d65b4e229df91d7944a
grossiValentina <grossivalentina435@gmail.com>
2024-01-12 18:52:27 +0100

    esercizio da finire

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import "./bootstrap";
+import "~resources/scss/app.scss";
+// Per permettere a vite di processare le immagini
+import.meta.glob(["../img/**"]);
+// Importiamo parte js di bootstrap css
+import * as bootstrap from 'bootstrap';
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import { resolve } from "path";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~resources': '/resources/',
+            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    }
 });
